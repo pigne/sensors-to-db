7ad2607f90783cd5f50781cc0c7a256861d7e68f
Yoann Pigné <yoann.pigne@gmail.com>
2017-01-23 18:04:03 +0100

    fix logs

--- mongodb.js
+++ mongodb.js
@@ -8,24 +8,24 @@ var logger = log4js.getLogger('dbLogger')
 
 // When successfully connected
 mongoose.connection.on('connected', function () {
-  logger.info('Connected to database at ' + config.databaseUrl);
+  logger.info('Connected to database at ' + config.db);
 });
 
 // If the connection throws an error
 mongoose.connection.on('error',function (err) {
-  logger.error('Database connection error: ' + err);
+  logger.error('Database connection error: ',  err);
   process.exit(0);
 });
 
 // When the connection is disconnected
 mongoose.connection.on('disconnected', function () {
-  logger.warn('Database connection disconnected');
+  logger.warn('Database connection lost');
 });
 
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', function() {
   mongoose.connection.close(function () {
-    logger.warn('Database connection disconnected through app termination');
+    logger.info('Disconnected from database through app termination');
     process.exit(0);
   });
 });
